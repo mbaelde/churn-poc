06778f613f5a432fdea2f759d0dd918e4d543359
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-10-12 10:49:22 +0200

    feat: init database

---
+++ database/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    customerID = Column(String, primary_key=True)
+    gender = Column(String)
+    SeniorCitizen = Column(Integer)
+    Partner = Column(String)
+    Dependents = Column(String)
+    tenure = Column(Integer)
+    PhoneService = Column(String)
+    MultipleLines = Column(String)
+    InternetService = Column(String)
+    OnlineSecurity = Column(String)
+    OnlineBackup = Column(String)
+    DeviceProtection = Column(String)
+    TechSupport = Column(String)
+    StreamingTV = Column(String)
+    StreamingMovies = Column(String)
+    Contract = Column(String)
+    PaperlessBilling = Column(String)
+    PaymentMethod = Column(String)
+    MonthlyCharges = Column(Float)
+    TotalCharges = Column(Float)
+    Churn = Column(String)
---
+++ database/init_db.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import pandas as pd
+from pathlib import Path
+import sqlite3
+
+import database
+
+database_dir = 'data/raw/telco_customer_churn.db'
+conn = sqlite3.connect(database_dir)
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS customers (
+        customerID TEXT PRIMARY KEY,
+        gender TEXT,
+        SeniorCitizen INTEGER,
+        Partner TEXT,
+        Dependents TEXT,
+        tenure INTEGER,
+        PhoneService TEXT,
+        MultipleLines TEXT,
+        InternetService TEXT,
+        OnlineSecurity TEXT,
+        OnlineBackup TEXT,
+        DeviceProtection TEXT,
+        TechSupport TEXT,
+        StreamingTV TEXT,
+        StreamingMovies TEXT,
+        Contract TEXT,
+        PaperlessBilling TEXT,
+        PaymentMethod TEXT,
+        MonthlyCharges REAL,
+        TotalCharges REAL,
+        Churn TEXT
+    )
+''')
+conn.close()
+
+Base = declarative_base()
+
+engine = create_engine(f'sqlite:///{database_dir}')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+data_dir = Path('data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+data = pd.read_csv(data_dir)
+data["TotalCharges"] = pd.to_numeric(data['TotalCharges'], errors='coerce')
+
+for _, row in data.iterrows():
+    customer = database.Customer(**row.to_dict())
+    session.add(customer)
+
+session.commit()
+
+session.close()
