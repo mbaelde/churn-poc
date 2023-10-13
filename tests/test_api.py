bc2ed32cc2dd8954780ae83f69eb733e2b1fff07
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-10-13 15:04:12 +0200

    refactor: update tests to use json data

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from fastapi.testclient import TestClient
@@ -10,27 +11,8 @@ client = TestClient(app)
 class TestAPI(unittest.TestCase):
     def test_predict_churn(self):
         # Define a sample request data
-        data = {
-            "gender": "Male",
-            "SeniorCitizen": 0,
-            "Partner": "Yes",
-            "Dependents": "No",
-            "tenure": 24,
-            "PhoneService": "Yes",
-            "MultipleLines": "No",
-            "InternetService": "DSL",
-            "OnlineSecurity": "Yes",
-            "OnlineBackup": "No",
-            "DeviceProtection": "No",
-            "TechSupport": "Yes",
-            "StreamingTV": "No",
-            "StreamingMovies": "No",
-            "Contract": "Month-to-month",
-            "PaperlessBilling": "No",
-            "PaymentMethod": "Mailed check",
-            "MonthlyCharges": 53.6,
-            "TotalCharges": 1315.35,
-        }
+        with open("data/example.json", "r") as f:
+            data = json.load(f)
 
         # Make a test request to the prediction endpoint
         response = client.post("/predict-churn", json=data)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,67 @@
+import json
 import unittest
-from models import ChurnModel
+from pathlib import Path
+
+import pandas as pd
+
+from api.models import ChurnModel
+
 
 class TestChurnModel(unittest.TestCase):
     def setUp(self):
         # Create an instance of the ChurnModel
-        self.churn_model = ChurnModel()
+        label_encoded_variables = [
+            "gender",
+            "Partner",
+            "Dependents",
+            "PhoneService",
+            "PaperlessBilling",
+            "Contract",
+            "PaymentMethod",
+            "TenureGroup",
+        ]
+        onehot_encoded_variables = [
+            "MultipleLines",
+            "InternetService",
+            "OnlineSecurity",
+            "OnlineBackup",
+            "DeviceProtection",
+            "TechSupport",
+            "StreamingTV",
+            "StreamingMovies",
+        ]
+        standard_scaler_variables = [
+            "tenure",
+            "MonthlyCharges",
+            "TotalCharges",
+            "MonthlyTotalChargesRatio",
+        ]
+        self.churn_model = ChurnModel(
+            label_encoded_variables=label_encoded_variables,
+            onehot_encoded_variables=onehot_encoded_variables,
+            standard_scaler_variables=standard_scaler_variables,
+        )
+        self.churn_model.deserialize(Path("data/processed"))
+
+        # Sample input data for prediction
+        with open("data/example.json", "r") as f:
+            self.data = json.load(f)
 
     def test_churn_prediction(self):
-        # Sample input data for prediction
-        data = {
-            "tenure": 24,
-            "MonthlyCharges": 65.0,
-            "TotalCharges": 1500.0,
-            # Add more features as needed
-        }
+
+        # Convert the input data to a Pandas DataFrame
+        input_data = pd.DataFrame([self.data])
+
+        # Perform the necessary preprocessing steps on the input data
+        # (e.g., encoding categorical variables and feature scaling)
+        X = self.churn_model.preprocess(input_data)
 
         # Make a prediction using the model
-        prediction = self.churn_model.predict(data)
+        prediction = self.churn_model.predict(X)
 
         # Validate the prediction
-        self.assertIn(prediction, ["Churn", "No Churn"])
+        self.assertIn(prediction, [0, 1])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
