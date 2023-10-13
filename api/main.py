f6ab67dcbb39153d04849617a174149d4fc0aee2
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-10-13 15:04:25 +0200

    refactor: update api to work

--- api/main.py
+++ api/main.py
@@ -2,10 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from api.routers.prediction import predict_churn
-from api.schemas.prediction import CustomerData
+from routers.prediction import predict_churn
+from schemas.prediction import CustomerData
 
 load_dotenv()
 
@@ -13,14 +11,6 @@ api_key = os.getenv("API_KEY")
 secret_key = os.getenv("SECRET_KEY")
 
 app = FastAPI()
-
-# Enable CORS to allow requests from any origin
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.post("/predict-churn")
--- api/routers/prediction.py
+++ api/routers/prediction.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pandas as pd
 
-import api.models as models
+import models as models
 
 # Initialize the model
 # Define the columns to be encoded
