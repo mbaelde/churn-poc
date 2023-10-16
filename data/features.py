8c96a35fef02ef5715b72caed72708229bac2d01
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-10-16 15:07:56 +0200

    feat: create a features module for preprocessing

---
+++ data/features.py
@@ -0,0 +1,334 @@
+import pickle
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+class FeaturePreprocessor:
+    """
+    FeaturePreprocessor is a class for fitting and transforming data using a feature preprocessing
+    model.
+
+    Args:
+        model: The feature preprocessing model to be used.
+        encoded_variables (List[str]): A list of encoded variable names.
+
+    Attributes:
+        _model: The feature preprocessing model.
+        _encoded_variables (List[str]): A list of encoded variable names.
+
+    Methods:
+        fit(X: pd.DataFrame) -> "FeaturePreprocessor":
+            Fit the feature preprocessing model on the provided DataFrame.
+
+        transform(X: pd.DataFrame) -> pd.DataFrame:
+            Transform the provided DataFrame using the fitted feature preprocessing model.
+
+        fit_transform(X: pd.DataFrame) -> pd.DataFrame:
+            Fit and transform the provided DataFrame using the feature preprocessing model.
+
+        serialize(feature_preprocessor_path: Path):
+            Serialize the feature preprocessing model to a binary file.
+
+        deserialize(feature_preprocessor_path: Path):
+            Deserialize the feature preprocessing model from a binary file.
+
+        get_feature_names(features: List[str]) -> List[str]:
+            Get the feature names after transformation, if available.
+
+    """
+
+    def __init__(self, model, encoded_variables: List[str]):
+        self._model = model
+        self._encoded_variables = encoded_variables
+
+    @property
+    def model(self):
+        """
+        The feature preprocessing model used for fitting and transforming data.
+        """
+        return self._model
+
+    def fit(self, X: pd.DataFrame) -> "FeaturePreprocessor":
+        """
+        Fit the feature preprocessing model on the provided DataFrame.
+
+        Args:
+            X (pd.DataFrame): The DataFrame containing the data to be transformed.
+
+        Returns:
+            FeaturePreprocessor: The fitted FeaturePreprocessor instance.
+
+        """
+        self._model.fit(X[self._encoded_variables])
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform the provided DataFrame using the fitted feature preprocessing model.
+
+        Args:
+            X (pd.DataFrame): The DataFrame to be transformed.
+
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+
+        """
+        return self._model.transform(X[self._encoded_variables])
+
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fit and transform the provided DataFrame using the feature preprocessing model.
+
+        Args:
+            X (pd.DataFrame): The DataFrame containing the data to be transformed.
+
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+
+        """
+        self.fit(X[self._encoded_variables])
+        return self.transform(X[self._encoded_variables])
+
+    def serialize(self, feature_preprocessor_path: Path):
+        """
+        Serialize the feature preprocessing model to a binary file.
+
+        Args:
+            feature_preprocessor_path (Path): The path to the binary file for serialization.
+
+        """
+        with open(feature_preprocessor_path, "wb") as f:
+            pickle.dump(self.model, f)
+
+    def deserialize(self, feature_preprocessor_path: Path):
+        """
+        Deserialize the feature preprocessing model from a binary file.
+
+        Args:
+            feature_preprocessor_path (Path): The path to the binary file for deserialization.
+
+        """
+        with open(feature_preprocessor_path, "rb") as f:
+            self._model = pickle.load(f)
+
+    def get_feature_names(self, features: List[str]) -> List[str]:
+        """
+        Get the feature names after transformation, if available.
+
+        Args:
+            features (List[str]): A list of feature names.
+
+        Returns:
+            List[str]: Transformed feature names if available, or an empty list.
+
+        """
+        try:
+            return self._model.get_feature_names(features)
+        except AttributeError:
+            return []
+
+class MultiColumnLabelEncoder:
+    def __init__(self, encoded_variables: List[str]):
+        """
+        Initialize a MultiColumnLabelEncoder.
+
+        Args:
+            encoded_variables (List[str]): A list of variable names to be encoded.
+
+        Returns:
+            None
+        """
+        self._encoded_variables = encoded_variables
+        self._model = {var: LabelEncoder() for var in encoded_variables}
+
+    def fit(self, X: pd.DataFrame) -> "MultiColumnLabelEncoder":
+        """
+        Fit the MultiColumnLabelEncoder.
+
+        Args:
+            X (pd.DataFrame): Input data with encoded variables.
+
+        Returns:
+            self: The MultiColumnLabelEncoder instance.
+        """
+        for var in self._encoded_variables:
+            self._model[var].fit(X[var])
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform data to encode the specified variables.
+
+        Args:
+            X (pd.DataFrame): Input data with variables to be encoded.
+
+        Returns:
+            pd.DataFrame: Transformed data with encoded variables.
+        """
+        data = X.copy()
+        for var in self._encoded_variables:
+            data[var] = self._model[var].transform(data[var])
+
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fit and transform the input data to encode the specified variables.
+
+        Args:
+            X (pd.DataFrame): Input data with variables to be encoded.
+
+        Returns:
+            pd.DataFrame: Transformed data with encoded variables.
+        """
+        self.fit(X)
+        return self.transform(X)
+
+    def serialize(self, feature_preprocessor_path: Path):
+        """
+        Serialize the encoding models to binary files.
+
+        Args:
+            feature_preprocessor_path (Path): Path to the binary files.
+
+        Returns:
+            None
+        """
+        for var in self._encoded_variables:
+            path = feature_preprocessor_path.with_name(
+                f"{feature_preprocessor_path.stem}_{var}{feature_preprocessor_path.suffix}"
+            )
+            with open(path, "wb") as f:
+                pickle.dump(self._model[var], f)
+
+    def deserialize(self, feature_preprocessor_path: Path):
+        """
+        Deserialize the encoding models from binary files.
+
+        Args:
+            feature_preprocessor_path (Path): Path to the binary files.
+
+        Returns:
+            None
+        """
+        for var in self._encoded_variables:
+            path = feature_preprocessor_path.with_name(
+                f"{feature_preprocessor_path.stem}_{var}{feature_preprocessor_path.suffix}"
+            )
+            with open(path, "rb") as f:
+                self._model[var] = pickle.load(f)
+
+
+class TenureBinarizer:
+    def __init__(self, bins, labels):
+        """
+        Initialize a TenureBinarizer.
+
+        Args:
+            bins: The bin edges for binning the tenure values.
+            labels: The labels for each tenure bin.
+
+        Returns:
+            None
+        """
+        self._bins = bins
+        self._labels = labels
+
+    def fit(self, X: pd.DataFrame) -> "TenureBinarizer":
+        """
+        Fit the TenureBinarizer.
+
+        Args:
+            X (pd.DataFrame): Input data (not used for fitting).
+
+        Returns:
+            self: The TenureBinarizer instance.
+        """
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform tenure values into bins and add a 'TenureGroup' column.
+
+        Args:
+            X (pd.DataFrame): Input data with a 'tenure' column.
+
+        Returns:
+            pd.DataFrame: Transformed data with 'TenureGroup' column added.
+        """
+        data = X.copy()
+        data["TenureGroup"] = pd.cut(
+            data["tenure"],
+            bins=self._bins,
+            labels=self._labels,
+        )
+        return data
+
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fit and transform the input data.
+
+        Args:
+            X (pd.DataFrame): Input data with a 'tenure' column.
+
+        Returns:
+            pd.DataFrame: Transformed data with 'TenureGroup' column added.
+        """
+        self.fit(X)
+        return self.transform(X)
+
+
+class RatioComputer:
+    def __init__(self, numerator: str, denominator: str, ratio_name: str):
+        """
+        Initialize a RatioComputer.
+
+        Args:
+            numerator (str): The name of the numerator column.
+            denominator (str): The name of the denominator column.
+            ratio_name (str): The name of the computed ratio column.
+
+        Returns:
+            None
+        """
+        self._numerator = numerator
+        self._denominator = denominator
+        self._ratio_name = ratio_name
+
+    def fit(self, X: pd.DataFrame) -> "RatioComputer":
+        """
+        Fit the RatioComputer.
+
+        Args:
+            X (pd.DataFrame): Input data (not used for fitting).
+
+        Returns:
+            self: The RatioComputer instance.
+        """
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Transform data to compute the specified ratio.
+
+        Args:
+            X (pd.DataFrame): Input data with appropriate columns.
+
+        Returns:
+            pd.DataFrame: Transformed data with the computed ratio added.
+        """
+        data = X.copy()
+        data[self._ratio_name] = data[self._numerator] / data[self._denominator]
+        return data
+
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fit and transform the input data to compute the specified ratio.
+
+        Args:
+            X (pd.DataFrame): Input data with appropriate columns.
+
+        Returns:
+            pd.DataFrame: Transformed data with the computed ratio added.
+        """
+        self.fit(X)
+        return self.transform(X)
--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,9 +1,16 @@
-import pickle
 import sqlite3
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from features import (
+    FeaturePreprocessor,
+    MultiColumnLabelEncoder,
+    RatioComputer,
+    TenureBinarizer,
+)
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 raw_data_dir = Path("data/raw")
 processed_data_dir = Path("data/processed")
@@ -34,45 +41,48 @@ nominal_cols = [
 ]
 
 # Handle missing values (if any)
-data["TotalCharges"].fillna(data["TotalCharges"].median(), inplace=True)
+imputer = SimpleImputer(strategy="median")
+data["TotalCharges"] = imputer.fit_transform(data[["TotalCharges"]])
 
 # Feature engineering
-data["TenureGroup"] = pd.cut(
-    data["tenure"],
-    bins=[0, 12, 24, 36, 48, 60, data["tenure"].max()],
-    labels=["0-1 Year", "1-2 Years", "2-3 Years", "3-4 Years", "4-5 Years", "5+ Years"],
+bins = [0, 12, 24, 36, 48, 60, np.inf]
+labels = ["0-1 Year", "1-2 Years", "2-3 Years", "3-4 Years", "4-5 Years", "5+ Years"]
+tenure_binarizer = TenureBinarizer(bins=bins, labels=labels)
+data = tenure_binarizer.fit_transform(data)
+
+ratio_computer = RatioComputer(
+    "MonthlyCharges", "TotalCharges", "MonthlyTotalChargesRatio"
 )
-print(data["tenure"].max())
-data["MonthlyTotalChargesRatio"] = data["MonthlyCharges"] / data["TotalCharges"]
+data = ratio_computer.fit_transform(data)
 
 # Feature scaling
-scaler = StandardScaler()
-data[
-    ["tenure", "MonthlyCharges", "TotalCharges", "MonthlyTotalChargesRatio"]
-] = scaler.fit_transform(
-    data[["tenure", "MonthlyCharges", "TotalCharges", "MonthlyTotalChargesRatio"]]
+standard_scaled_variables = [
+    "tenure",
+    "MonthlyCharges",
+    "TotalCharges",
+    "MonthlyTotalChargesRatio",
+]
+scaler = FeaturePreprocessor(
+    model=StandardScaler(), encoded_variables=standard_scaled_variables
 )
-with open(processed_data_dir.joinpath("standard_scaler.pkl"), "wb") as scaler_file:
-    pickle.dump(scaler, scaler_file)
+data[standard_scaled_variables] = scaler.fit_transform(data[standard_scaled_variables])
+scaler.serialize(processed_data_dir.joinpath("standard_scaler.pkl"))
 
 # Initialize encoders
-label_encoder = {col: LabelEncoder() for col in binary_cols + ordinal_cols}
-onehot_encoder = OneHotEncoder(
-    sparse=False, drop="first"
-)  # 'drop' is set to 'first' to avoid multicollinearity
-
-# Label encoding for binary and ordinal variables
-for col in binary_cols + ordinal_cols:
-    data[col] = label_encoder[col].fit_transform(data[col])
-    with open(processed_data_dir.joinpath(f"label_encoder_{col}.pkl"), "wb") as le_file:
-        pickle.dump(label_encoder[col], le_file)
+label_encoder_variables = binary_cols + ordinal_cols
+label_encoder = MultiColumnLabelEncoder(encoded_variables=label_encoder_variables)
+data[label_encoder_variables] = label_encoder.fit_transform(data)
+label_encoder.serialize(processed_data_dir.joinpath(f"label_encoder.pkl"))
 
 # One-hot encoding for nominal variables
-nominal_data = onehot_encoder.fit_transform(data[nominal_cols])
+onehot_encoder = FeaturePreprocessor(
+    model=OneHotEncoder(sparse=False, drop="first"), encoded_variables=nominal_cols
+)  # 'drop' is set to 'first' to avoid multicollinearity
+
+nominal_data = onehot_encoder.fit_transform(data)
 nominal_columns = onehot_encoder.get_feature_names(nominal_cols)
 nominal_df = pd.DataFrame(nominal_data, columns=nominal_columns)
-with open(processed_data_dir.joinpath(f"onehot_encoder.pkl"), "wb") as ohe_file:
-    pickle.dump(onehot_encoder, ohe_file)
+onehot_encoder.serialize(processed_data_dir.joinpath(f"onehot_encoder.pkl"))
 
 # Combine the one-hot encoded nominal variables with the rest of the dataset
 data_encoded = pd.concat([data.drop(columns=nominal_cols), nominal_df], axis=1)
