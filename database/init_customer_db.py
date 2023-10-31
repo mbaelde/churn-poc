7ef3517560a625344d5584c830c3081aefe095f9
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-10-31 10:55:10 +0100

    refactor: change the database schema

--- database/init_customer_db.py
+++ database/init_customer_db.py
@@ -1,60 +1,69 @@
-import sqlite3
 from pathlib import Path
 
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from database.database import Customer
-
-database_dir = "data/raw/telco_customer_churn.db"
-conn = sqlite3.connect(database_dir)
-cursor = conn.cursor()
-
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS customers (
-        customerID TEXT PRIMARY KEY,
-        gender TEXT,
-        SeniorCitizen INTEGER,
-        Partner TEXT,
-        Dependents TEXT,
-        tenure INTEGER,
-        PhoneService TEXT,
-        MultipleLines TEXT,
-        InternetService TEXT,
-        OnlineSecurity TEXT,
-        OnlineBackup TEXT,
-        DeviceProtection TEXT,
-        TechSupport TEXT,
-        StreamingTV TEXT,
-        StreamingMovies TEXT,
-        Contract TEXT,
-        PaperlessBilling TEXT,
-        PaymentMethod TEXT,
-        MonthlyCharges REAL,
-        TotalCharges REAL,
-        Churn TEXT
-    )
-"""
+from database.models import (
+    Base,
+    Contract,
+    Customer,
+    CustomerChurn,
+    InternetService,
+    PhoneService,
 )
-conn.close()
 
-Base = declarative_base()
+# Create an SQLite database (You can use a different database URL if needed)
+database_url = "sqlite:///customers.db"
+engine = create_engine(database_url)
 
-engine = create_engine(f"sqlite:///{database_dir}")
+# Create tables in the database
 Base.metadata.create_all(engine)
 
+# Initialize a session to interact with the database
 Session = sessionmaker(bind=engine)
 session = Session()
 
 data_dir = Path("data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 data = pd.read_csv(data_dir)
 data["TotalCharges"] = pd.to_numeric(data["TotalCharges"], errors="coerce")
+data["SeniorCitizen"] = data["SeniorCitizen"].map({0: "No", 1: "Yes"})
 
 for _, row in data.iterrows():
-    customer = Customer(**row.to_dict())
+    phone_service = PhoneService(
+        hasPhoneService=row["PhoneService"], multipleLines=row["MultipleLines"]
+    )
+    internet_service = InternetService(
+        internetServiceType=row["InternetService"],
+        onlineSecurity=row["OnlineSecurity"],
+        onlineBackup=row["OnlineBackup"],
+        deviceProtection=row["DeviceProtection"],
+        techSupport=row["TechSupport"],
+        streamingTV=row["StreamingTV"],
+        streamingMovies=row["StreamingMovies"],
+    )
+    contract = Contract(
+        contractType=row["Contract"],
+        tenure=row["tenure"],
+        paperlessBilling=row["PaperlessBilling"],
+        paymentMethod=row["PaymentMethod"],
+        monthlyCharges=row["MonthlyCharges"],
+        totalCharges=row["TotalCharges"],
+        phone_service=phone_service,
+        internet_service=internet_service,
+    )
+    customer_churn = CustomerChurn(churn=row["Churn"])
+
+    customer = Customer(
+        id=row["customerID"],
+        gender=row["gender"],
+        seniorCitizen=row["SeniorCitizen"],
+        partner=row["Partner"],
+        dependents=row["Dependents"],
+        contracts=[contract],
+        churns=[customer_churn],
+    )
+
     session.add(customer)
 
 session.commit()
--- database/models.py
+++ database/models.py
@@ -1,57 +1,103 @@
-from sqlalchemy import Column, Float, Integer, String, func
-from sqlalchemy.ext.declarative import declarative_base
+from typing import List, Optional
 
-Base = declarative_base()
+from sqlalchemy import Float, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+# Define the Customer table
 class Customer(Base):
-    __tablename__ = "customers"
+    __tablename__ = "Customer"
 
-    customerID = Column(String, primary_key=True)
-    gender = Column(String)
-    SeniorCitizen = Column(Integer)
-    Partner = Column(String)
-    Dependents = Column(String)
-    tenure = Column(Integer)
-    PhoneService = Column(String)
-    MultipleLines = Column(String)
-    InternetService = Column(String)
-    OnlineSecurity = Column(String)
-    OnlineBackup = Column(String)
-    DeviceProtection = Column(String)
-    TechSupport = Column(String)
-    StreamingTV = Column(String)
-    StreamingMovies = Column(String)
-    Contract = Column(String)
-    PaperlessBilling = Column(String)
-    PaymentMethod = Column(String)
-    MonthlyCharges = Column(Float)
-    TotalCharges = Column(Float)
-    Churn = Column(String)
+    id: Mapped[str] = mapped_column(primary_key=True, unique=True)
+    gender: Mapped[str] = mapped_column(String)
+    seniorCitizen: Mapped[str] = mapped_column(String)
+    partner: Mapped[str] = mapped_column(String)
+    dependents: Mapped[str] = mapped_column(String)
+
+    # Define the one-to-one relationship between Customer and Contract
+    contracts: Mapped[List["Contract"]] = relationship(back_populates="customer")
+    churns: Mapped[List["CustomerChurn"]] = relationship(back_populates="customer")
+
+    def __repr__(self) -> str:
+        return f"Customer(id={self.id!r}, gender={self.gender!r}, seniorCitizen={self.seniorCitizen!r}, partner={self.partner!r}, dependents={self.dependents!r})"
 
 
+# Define the PhoneService table
+class PhoneService(Base):
+    __tablename__ = "PhoneService"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    hasPhoneService: Mapped[str] = mapped_column(String)
+    multipleLines: Mapped[str] = mapped_column(String)
+
+    contract_id: Mapped[int] = mapped_column(ForeignKey("Contract.id"))
+    contract: Mapped["Contract"] = relationship(back_populates="phone_service")
+
+    def __repr__(self) -> str:
+        return f"PhoneService(id={self.id!r}, hasPhoneService={self.hasPhoneService!r}, multipleLines={self.multipleLines!r})"
+
+
+# Define the InternetService table
+class InternetService(Base):
+    __tablename__ = "InternetService"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    internetServiceType: Mapped[str] = mapped_column(String)
+    onlineSecurity: Mapped[str] = mapped_column(String)
+    onlineBackup: Mapped[str] = mapped_column(String)
+    deviceProtection: Mapped[str] = mapped_column(String)
+    techSupport: Mapped[str] = mapped_column(String)
+    streamingTV: Mapped[str] = mapped_column(String)
+    streamingMovies: Mapped[str] = mapped_column(String)
+
+    contract_id: Mapped[int] = mapped_column(ForeignKey("Contract.id"))
+    contract: Mapped["Contract"] = relationship(back_populates="internet_service")
+
+    def __repr__(self) -> str:
+        return f"InternetService(id={self.id!r}, internetServiceType={self.internetServiceType!r}, onlineSecurity={self.onlineSecurity!r}, onlineBackup={self.onlineBackup!r}, deviceProtection={self.deviceProtection!r}, techSupport={self.techSupport!r}, streamingTV={self.streamingTV!r}, streamingMovies={self.streamingMovies!r})"
+
+
+# Define the Contract table
+class Contract(Base):
+    __tablename__ = "Contract"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    contractType: Mapped[str] = mapped_column(String)
+    tenure: Mapped[int] = mapped_column(Integer)
+    paperlessBilling: Mapped[str] = mapped_column(String)
+    paymentMethod: Mapped[str] = mapped_column(String)
+    monthlyCharges: Mapped[float] = mapped_column(Float)
+    totalCharges: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+
+    customer_id: Mapped[int] = mapped_column(ForeignKey("Customer.id"))
+    customer: Mapped["Customer"] = relationship(back_populates="contracts")
+
+    # Define the one-to-one relationship between Contract and PhoneService
+    phone_service: Mapped["PhoneService"] = relationship(back_populates="contract")
+
+    # Define the one-to-one relationship between Contract and InternetService
+    internet_service: Mapped["InternetService"] = relationship(
+        back_populates="contract"
+    )
+
+    def __repr__(self) -> str:
+        return f"Contract(id={self.id!r}, contractType={self.contractType!r}, tenure={self.tenure!r}, paperlessBilling={self.paperlessBilling!r}, paymentMethod={self.paymentMethod!r}, monthlyCharges={self.monthlyCharges!r}, totalCharges={self.totalCharges!r})"
+
+
+# Define the Churn table
 class CustomerChurn(Base):
-    __tablename__ = "customer_churn"
+    __tablename__ = "CustomerChurn"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customerID = Column(String, unique=True, index=True)
-    churn_prediction = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
+    churn: Mapped[str] = mapped_column(String)
 
-def create_customer(db, customerID: str, churn_prediction: str):
-    id_max = db.query(func.max(CustomerChurn.id)).scalar()
-    if id_max is None:
-        id_max = -1
-    else:
-        id_max = int(id_max)
-    db_customer = CustomerChurn(
-        id=id_max + 1, customerID=customerID, churn_prediction=churn_prediction
-    )
-    db.add(db_customer)
-    db.commit()
+    customer_id: Mapped[int] = mapped_column(ForeignKey("Customer.id"))
+    customer: Mapped["Customer"] = relationship(back_populates="churns")
 
-
-def get_customer(db, customerID: str):
-    return (
-        db.query(CustomerChurn).filter(CustomerChurn.customerID == customerID).first()
-    )
+    def __repr__(self) -> str:
+        return f"CustomerChurn(id={self.id!r}, churn={self.churn!r})"
