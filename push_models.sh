dc408f972429b64f8f76a5b0483fb14f63aeda41
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-11-02 15:23:08 +0100

    refactor: update push_model script

--- push_models.sh
+++ push_models.sh
@@ -2,6 +2,6 @@ gcloud auth login
 
 gcloud config set project churn-prediction-poc
 
-gsutil cp data/models/*.pkl gs://models-churn/models/
-gsutil cp data/preprocessors/*.pkl gs://models-churn/preprocessors/
+gsutil -m cp data/models/*.pkl gs://models-churn/models/
+gsutil -m cp data/preprocessors/*.pkl gs://models-churn/preprocessors/
 
